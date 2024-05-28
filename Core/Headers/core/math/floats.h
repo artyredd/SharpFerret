d8f2e8b3b7564115985275b7ed6c8303f91f7e14
artyredd <canadianspycrab@gmail.com>
2024-05-28 03:10:55 -0400

    Add runtime loop functionality

--- Core/Headers/core/math/floats.h
+++ Core/Headers/core/math/floats.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "core/csharp.h"
 #include "core/file.h"
+#include <float.h>
+
+#define DEFAULT_EPSILON 0.00001
+#define approximate(left,right) (fabs(left-right) < DEFAULT_EPSILON)
 
 struct _floatMethods {
 	bool (*TryDeserialize)(const char* buffer, ulong bufferLength, float* out_float);
